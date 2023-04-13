db5aae7e8b2681177389d134a173d16584bc67f2
RennyCode <56790787+RennyCode@users.noreply.github.com>
2023-04-13 23:52:52 +0300

    7

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask ,render_template, redirect, request, session, url_for, session
-import os
-import numpy as np
-import matplotlib
+from flask import Flask ,render_template, redirect, request, session, url_for, session, send_file, flash, Response
+import os, glob, re, cv2, matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-import cv2
 import classes as cl
 from stable_baselines3 import DQN
 from flask_executor import Executor
-
 
 app = Flask(__name__)
 app.secret_key = os.urandom(100)
@@ -17,27 +13,25 @@ app.config['EXECUTOR_TYPE'] = 'thread'
 app.config['EXECUTOR_MAX_WORKERS'] = 5
 env = None
 model = None
+timesteps = 1
 
-callback = cl.TrainAndLoggingCallback(check_freq=500, save_path=cl.CHECKPOINT_DIR)
+# callback = cl.TrainAndLoggingCallback(check_freq=10000, save_path=cl.CHECKPOINT_DIR)
+callback = cl.SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=cl.CHECKPOINT_DIR)
 
 @app.route('/' ,methods=['GET','POST'])
 def form_post():
     global env
+    global model
     if request.method == 'POST':
+        env = cl.game()
         if request.form['submit_button'] == 'sub-bt':
-        # if 'sub-bt' in request.form:
-            box_top_pos_input = request.form["box-top-pos-input"]
-            box_Left_input = request.form["box-left-input"]
-            box_width_input = request.form["box-width-input"]
-            box_hight_input = request.form["box-hight-input"]
-            done_top_pos_input = request.form["done-top-pos-input"]
-            done_Left_input = request.form["done-left-input"]
-            done_width_input = request.form["done-width-input"]
-            done_hight_input = request.form["done-hight-input"]
-            session['box'] = [box_top_pos_input ,box_Left_input, box_width_input, box_hight_input]
-            session['done'] = [done_top_pos_input ,done_Left_input, done_width_input, done_hight_input]
-            # env = game(box, done)
-            env = cl.game()
+
+            session['box'] = [int(request.form["box-top-pos-input"]) , int(request.form["box-left-input"]), int(request.form["box-width-input"]), int(request.form["box-hight-input"])]
+            session['done'] = [int(request.form["done-top-pos-input"]) ,int(request.form["done-left-input"]), int(request.form["done-width-input"]), int(request.form["done-hight-input"])]
+            keys_input = request.form["keys-input"].split(",")
+            session['keys'] = [key for key in keys_input]
+            session['res-seq'] = request.form["reset-input"].split(",")
+            session['neut-pos'] = [int(request.form["x_coordinate"]), int(request.form["y_coordinate"])]
             plt.imshow(env.get_observation()[0])
             plt.savefig(cl.b1_path)
             plt.imshow(env.get_done_observation()[0])
@@ -46,42 +40,69 @@ def form_post():
             plt.savefig(cl.b2_path)
             plt.imshow(cv2.cvtColor(env.get_done_observation()[0], cv2.COLOR_BGR2RGB))
             plt.savefig(cl.d2_path)
+            env = cl.game(session['box'], session['done'], session['keys'], session['res-seq'], session['neut-pos']) 
             return redirect(url_for('.sample')) 
-            # return redirect('/sample.html', box=box, done=done) 
-        else:
-
-            all_windows = cl.get_all_window_titles()
-            print(all)
-            return render_template('/home.html', all_windows=all_windows)
-
+        
+        elif request.form['submit_button'] == 'dino-bt':
+            env = cl.game(gameloc = [300, 380, 750, 460], doneloc = [425, 650, 650, 70], actions = ['no_op','space', 'down'], rest_seq = [ 'click','up', 'up', 'click', 'space'], neutral_click_pos = [150, 250])
+            return redirect(url_for('.model_training'))
+        
+        elif request.form['submit_button'] == 'upload-bt':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return render_template('/home.html')
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return render_template('/home.html')
+            print('this should save!')
+            latest_model_number  = '1'
+            file_name = os.path.join(cl.model_path, 'DQN1')
+            if os.listdir(cl.model_path):
+                latest_model_folder = max(glob.glob(cl.model_path + '/*'), key=os.path.getmtime)
+                latest_model_numbers_list  = re.findall(r'\d+', latest_model_folder.split(cl.folder_base_name)[-1])
+                latest_model_number = int(''.join(latest_model_numbers_list)) + 1
+                file_name = os.path.join(cl.model_path, 'DQN' + str(latest_model_number))
+            # file.save(os.path.join(cl.model_path, file_name))
+            print(file_name + '.zip')
+            file.save(file_name + '.zip')
+            model = DQN.load(file_name, env=env)
+            return redirect(url_for('.model_testing')) 
         
     else: 
-        return render_template('/home.html', all_windows=None)
+        return render_template('/home.html')
     
     
 @app.route('/sample' ,methods=['GET','POST'])
 def sample():
-    box = session.get('box')
-    done = session.get('done')
+
     if request.method == 'POST':
         if request.form['submit_button'] == 'conti-bt':
             return redirect(url_for('.model_training'))
         else:
             return redirect(url_for('.form_post'))
-    return render_template('/sample.html', box=box, done=done)
+    return render_template('/sample.html', box=session.get('box'), done=session.get('done'), keys=session.get('keys'), res_seq=session.get('res-seq'), neut_pos=session.get('neut-pos'))
 
 
 @app.route('/training' ,methods=['GET','POST'])
 def model_training():
     global model
     if request.method == 'POST':
-        # cl.random_play(env)
-        model.learn(total_timesteps=5, callback=callback)
+        model.learn(total_timesteps=timesteps, callback=callback)
 
-        return render_template('/testing.html')
+        if  not os.listdir(cl.model_path):
+            model.save(os.path.join(cl.model_path, 'DQN1'))
+        else:
+            latest_model_folder = max(glob.glob(cl.model_path + '/*'), key=os.path.getmtime)
+            latest_model_numbers_list  = re.findall(r'\d+', latest_model_folder.split(cl.folder_base_name)[-1])
+            latest_model_number = int(''.join(latest_model_numbers_list)) + 1
+            model.save(os.path.join(cl.model_path, 'DQN' + str(latest_model_number)))
+
+        return redirect(url_for('model_testing'))
     else:
-        model = DQN('CnnPolicy', env, tensorboard_log=cl.LOG_DIR, verbose=1,
-                    buffer_size= 150000, learning_starts= 100)
+        model = DQN('CnnPolicy', env, tensorboard_log=cl.LOG_DIR, verbose=1, buffer_size= 150000, learning_starts= 100)
         return render_template('/training.html')
     
 
@@ -90,5 +111,20 @@ def model_training():
 def model_testing():
     global model
     if request.method == 'POST':
-       cl.testing_model(env, model)
+       
+        if request.form['submit_button'] == 'redo-bt':
+            return redirect(url_for('.form_post'))
+        elif request.form['submit_button'] == 'test-bt':
+            cl.testing_model(env, model)   
+            return render_template('/testing.html')
+        elif request.form['submit_button'] == 'downlad-bt"':
+            pass
+            
     return render_template('/testing.html')
+
+
+
+@app.route('/download')
+def download_model():
+    file_path = max(glob.glob(cl.model_path + '/*'), key=os.path.getmtime)
+    return send_file(file_path, as_attachment=True)
--- classes.py
+++ classes.py
@@ -1,10 +1,8 @@
 from gym import Env
 from gym.spaces import Box, Discrete
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
 from stable_baselines3.common import env_checker
-from stable_baselines3 import DQN
-import pyautogui
-import pygetwindow
 import time
 import pydirectinput
 import cv2
@@ -18,15 +16,18 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 # DIR
 CHECKPOINT_DIR = './train/'
 LOG_DIR = './logs/'
+MODEL_DIR = '/models'
 # paths
 b1_path = os.getcwd() + '/static/images/box1.png'
 d1_path = os.getcwd() + '/static/images/done1.png'
 b2_path = os.getcwd() + '/static/images/box2.png'
 d2_path = os.getcwd() + '/static/images/done2.png'
+model_path = os.getcwd() + MODEL_DIR
+folder_base_name = 'DNQ'
+dir_mode = 0o666
 
-
-def get_all_window_titles():
-    return [win.title for win in pygetwindow.getAllWindows()]
+# def get_all_window_titles():
+#     return [win.title for win in pygetwindow.getAllWindows()]
 
 
 def testing_model(env, model):
@@ -52,26 +53,65 @@ def random_play(env):
                 total_reward += reward
             print(f'total reward for ep {ep} is {total_reward}')
 
-class TrainAndLoggingCallback(BaseCallback):
-    
-    def __init__(self, check_freq, save_path, verbose=1):
-        super(TrainAndLoggingCallback, self).__init__(verbose)
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
         self.check_freq = check_freq
-        self.save_path = save_path
-        
-    def _init_callback(self):
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, "best_model")
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
         if self.save_path is not None:
             os.makedirs(self.save_path, exist_ok=True)
-            
-    def _on_step(self):
+
+    def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
-            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
-            self.model.save(model_path)
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), "timesteps")
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose >= 1:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose >= 1:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
+        return True
+
+# class TrainAndLoggingCallback(BaseCallback):
+    
+#     def __init__(self, check_freq, save_path, verbose=1):
+#         super(TrainAndLoggingCallback, self).__init__(verbose)
+#         self.check_freq = check_freq
+#         self.save_path = save_path
+        
+#     def _init_callback(self):
+#         if self.save_path is not None:
+#             pass
+#             os.makedirs(self.save_path, exist_ok=True)
+            
+#     def _on_step(self):
+#         pass
+#         if self.n_calls % self.check_freq == 0:
+#             model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+#             self.model.save(model_path)
 
 
 class game(Env):
     # set env action and obser shaps
-    def __init__(self, gameloc = [200, 530, 450, 180], doneloc = [240, 800, 380, 40]):
+    def __init__(self, gameloc = [300, 380, 750, 460], doneloc = [425, 650, 650, 70], actions = ['no_op','space', 'down'], rest_seq = [ 'click','up', 'up', 'click', 'space'], neutral_click_pos = [150, 250]):
         # use the base class
         super().__init__()
         # setup spaces
@@ -81,15 +121,26 @@ class game(Env):
         self.cap = mss()
         self.game_location = {'top':gameloc[0], 'left':gameloc[1], 'width':gameloc[2], 'height':gameloc[3]}
         self.done_location = {'top':doneloc[0], 'left':doneloc[1], 'width':doneloc[2], 'height':doneloc[3]}
-        
+        # create action dic
+        if 'no_op' not in actions:
+            actions.insert(0, 'no_op')
+        else:
+            actions.insert(0, actions.pop(actions.index('no_op')))
+        self.action_map = {v: k for v, k in enumerate(actions)}
+        # set rest_seq
+        self.rest_seq = rest_seq
+        self.neutral_click_pos = neutral_click_pos
+
+
     # called to do somehitng in the game     
     def step(self, action):
+        
         # actions: 0 - space(jump), 1 - down(duck), 2 - no action(no op) 
-        action_map = {0: 'space', 1: 'down', 2: 'no_op'}
-    
-        # simulate press if not no_op
-        if action != 2:
-            pydirectinput.press(action_map[action])
+        # action_map = {1: 'space', 2: 'down', 0: 'no_op'}
+
+        # simulate press if not 
+        if action != 0:
+            pydirectinput.press(self.action_map[action])
     
         # check for gameover
         done, done_cap = self.get_done()
@@ -100,20 +151,25 @@ class game(Env):
         #info dic
         info = {}
         return new_observ, reward, done, info 
+    
     # vusualize the game
     def render(self):
         cv2.imshow('Game', np.array(self.cap.grab(self.game_location))[:,:,:3])
         if cv2.waitKey(1) & 0xFF == ord('q'):
             self.close()
-    # closes the done observ
+
+    # closes the done 
     def close(self):
         cv2.destroyAllWindows()
+        
     # restart the game     
     def reset(self):
         time.sleep(1)
-        pydirectinput.press('up')
-        pydirectinput.click(x=150, y=150)
-        pydirectinput.press('space')
+        for action in self.rest_seq:
+            if action == "click":
+                pydirectinput.click(x=self.neutral_click_pos[0], y=self.neutral_click_pos[1])
+            else:
+                pydirectinput.press(action)
         time.sleep(2)
         return self.get_observation()
 
